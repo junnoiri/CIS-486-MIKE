8121d9af6b6403441595524df7258df164a4b9d1
Jun Noiri <63932655+junnoiri@users.noreply.github.com>
2023-08-22 13:36:55 -0500

    fix client and server files

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
-// const express = require("express");
-// const app = express();
-// const port = 3000;
-
-// app.get("/", (req, res) => {
-//   res.send("Hello, World Node!!");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
-// CORSミドルウェアを使用
 app.use(cors());
 
-// ルートURLにアクセスがあった場合にレスポンスを返す
 app.get("/", (req, res) => {
   res.send("Hello, Server!");
 });
